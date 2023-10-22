fd79d0fe271b00fdad06d432ad356e6391a42d74
zxc_ghoul <AlexisV.Shishkin@gmail.com>
2023-10-22 19:07:13 +0700

    fixed 2, 3

--- sem9/2.c
+++ sem9/2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 int* findSubArr(int* arr1, int len1, int* arr2, int len2) {
     for (int i = 0; i <= len1 - len2; i++) {
         int found = 1;
@@ -23,9 +24,8 @@ int main() {
     int len1 = sizeof(arr1) / sizeof(arr1[0]);
     int len2 = sizeof(arr2) / sizeof(arr2[0]);
 
-
     int* result = findSubArr(arr1, len1, arr2, len2);
-    if (result != 0) {
+    if (result != NULL) {
         printf("index: %d\n", result - arr1);
     } else {
         printf("Not found\n");
--- sem9/3.c
+++ sem9/3.c
@@ -1,70 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-
-void nullChek(void* ptr){
-    if (ptr == NULL){
-        printf("Out of memory!");
-        exit(0);
-    }
-}
-
-int* findSubArray(int arr1[], int len, int arr2[], int len2, int* result) {
-    int start_subarr;
-    size_t c = 0;
-    for (size_t i = 0; i < len; i++) {
-        if (c == len2){
-            *result = start_subarr - len2 + 1;
-            return  &(arr1[start_subarr - len2 + 1]);
+int* findSubArr(int* arr1, int len1, int* arr2, int len2) {
+    for (int i = 0; i <= len1 - len2; i++) {
+        int found = 1;
+        for (int j = 0; j < len2; j++) {
+            if (arr1[i + j] != arr2[j]) {
+                found = 0;
+                break;
+            }
         }
-        if (arr1[i] == arr2[c]){
-            start_subarr = i;
-            c++;
+        if (found) {
+            return &arr1[i];
         }
-        else
-            c = 0;
     }
     return 0;
 }
 
-void RemoveEl(int* arr, int* len, size_t index){
-    for (size_t i = 0; i < *len - index - 1; i++){
-        arr[index + i] = arr[index + 1 + i];
-    }
-    *len -= 1;
-}
 
-void removeSubArr(int* arr1[], int len, int arr2[], int len2, int* resLen){
-    int res;
-    findSubArray(arr1, len, arr2, len2, &res);
-    do{
-        for (size_t i = 0; i < len2; i++){
-            RemoveEl(arr1, &len, res);
+void removeSubArr(int* arr1, int* len1, int* arr2, int len2) {
+    int* found;
+    while ((found = findSubArr(arr1, *len1, arr2, len2)) != NULL) {
+        for (int *p = found; p < arr1 + *len1 - len2; p++) {
+            *p = *(p + len2);
         }
-    } while (findSubArray(arr1, len, arr2, len2, &res) != 0);
-    *resLen = len;
-
+        *len1 -= len2;
+    }
 }
 
 
 int main() {
-    int arr[9] = { 0, 1, 8, 7, 4, 1, 8, 7, 6};
-    int arr2[2] = {8, 7};
+    int arr1[] = {0, 1, 2, 3, 1, 2, 4};
+    int arr2[] = {1, 2};
+    int len1 = sizeof(arr1) / sizeof(arr1[0]);
+    int len2 = sizeof(arr2) / sizeof(arr2[0]);
 
-    size_t len = 9;
-    int* arr1 = (int*) malloc(len * sizeof(int));
-    nullChek(arr1);
-    for (size_t i = 0; i < len; i++){
-        arr1[i] = arr[i];
-    }
+    removeSubArr(arr1, &len1, arr2, len2);
 
-    int res;
-    removeSubArr(arr1, 9, arr2, 2, &res);
-    arr1 = realloc(arr1, res * sizeof(int));
-    nullChek(arr1);
-    for (size_t i = 0; i < res; i++){
+    printf("arr1: ");
+    for (int i = 0; i < len1; i++) {
         printf("%d ", arr1[i]);
     }
-    free(arr1);
+    printf("\n");
+
     return 0;
 }
